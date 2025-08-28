78d25947174448dd69a952d6bc3617f22de2c0f7
Msierra001 <msierra001@citymail.cuny.edu>
2025-08-28 19:26:41 -0400

    Adding comments to help with my understanding of the code

--- C++17_In_Detail/Chapter Clarification/copy_elision.cpp
+++ C++17_In_Detail/Chapter Clarification/copy_elision.cpp
@@ -9,12 +9,26 @@
 #include <iostream>
 
 struct Test {
+    /* Constructor(s) */
+    
+    // Default constructor 
     Test() { std::cout << "Test::Test\n"; }
+    
+    // Copy constructor: runs when copying an existing Test (e.g. Test b = a;)
     Test(const Test&) { std::cout << "Test(const Test&)\n"; }
+
+    // Move construcotr: runs when taking ownership from a temporary Test (e.g Test b = Test();)
+    // '&&' means this constructor takes an rvalue reference.
+    // An rvalue is a temporary object or a value that doesn’t have a persistent memory address, e.g., a literal or 
+    // a returned temporary.
     Test(Test&&) { std::cout << "Test(Test&&)\n"; }
+
+    
+    // Destructor 
     ~Test() { std::cout << "~Test\n"; }
 };
 
+// Creating a Test object with the default Test constructor and returning it. 
 Test Create() {
     return Test();
 }
@@ -22,3 +36,14 @@ Test Create() {
 int main() {
     auto n = Create();
 }
+
+/*
+    In the above call, you might assume a temporary copy is used - to store the return value of Create. 
+    In C++14, most compilers can see that the temporary object can be easily optimised and they can 
+    create n “directly” from the call of Create(). So you’ll probably see the following output:
+
+    Test::Test // create n
+    ~Test // destroy n when main finishes
+*/
+
+
