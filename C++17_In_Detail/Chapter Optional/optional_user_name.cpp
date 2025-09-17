7b43ef49378b1433c5bf7a4ee9e7113261829d9d
Msierra001 <melvin.sierraic3@gmail.com>
2025-09-17 18:44:31 -0400

    Adding clarifying comments

--- C++17_In_Detail/Chapter Optional/optional_user_name.cpp
+++ C++17_In_Detail/Chapter Optional/optional_user_name.cpp
@@ -3,40 +3,56 @@
 // by Bartlomiej Filipek
 // 2018/2019
 
-#include <optional>
-#include <iostream>
+#include <optional>   // for std::optional
+#include <iostream>   // for std::cout, std::ostream
 
+// A simple class to represent a user record that may or may not have a nickname or age
 class UserRecord {
 public:
+    // Constructor:
+    // Takes a required 'name' and two optional values: 'nick' and 'age'
+    // std::move(name) avoids unnecessary copies
     UserRecord (std::string name, std::optional<std::string> nick, std::optional<int> age)
     : mName{std::move(name)}, mNick{std::move(nick)}, mAge{age}
     {
     }
 
+    // Overload operator<< so that we can directly print UserRecord with std::cout
     friend std::ostream& operator << (std::ostream& stream, const UserRecord& user);
 
 private:
-    std::string mName;
-    std::optional<std::string> mNick;
-    std::optional<int> mAge;
+    std::string mName;              // always required
+    std::optional<std::string> mNick; // optional nickname
+    std::optional<int> mAge;          // optional age
 };
 
+// Output operator definition
 std::ostream& operator << (std::ostream& os, const UserRecord& user) {
-    os << user.mName;
+    os << user.mName;  // always print the name
     
+    // Check if nickname is present
     if (user.mNick)
-        os << ' ' << *user.mNick;
+        os << ' ' << *user.mNick;   // dereference optional to access its value
 
+    // Check if age is present
     if (user.mAge)
         os << ' ' << "age of " << *user.mAge;
-        
+
     return os;
 }
 
 int main() {
+    // User with all fields set
     UserRecord tim { "Tim", "SuperTim", 16 };
+
+    // User with only a name, no nickname or age (set to std::nullopt)
     UserRecord nano { "Nathan", std::nullopt, std::nullopt };
     
+    // Printing will adapt automatically:
+    // Tim SuperTim age of 16
+    // Nathan
     std::cout << tim << '\n';
     std::cout << nano << '\n';
+
+    return 0;   // (optional in modern compilers, but here for clarity)
 }
